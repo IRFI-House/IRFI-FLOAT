fdbc0e862bc09e77c4c0aa1c1f8d53eb025f1b16
✦ IRFI ✦ <irfihouse@gmail.com>
2025-07-16 02:53:01 +0200

    Release V1

---
+++ The_Float/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game { 'gta5' }
+lua54 'yes'
+-------------------------------------------------------------------
+author 'IRFI House'
+description 'The Float [Collaborative Project] - Last conctribution : IRFI House'
+version '1.0'
+this_is_a_map 'yes'
